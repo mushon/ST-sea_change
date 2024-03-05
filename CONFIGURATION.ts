4115f2de3978faab35cef320e283519a23e24c43
Adam Kariv <akariv@users.noreply.github.com>
2024-03-05 12:18:21 +0200

    Update airtable api key

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -1,5 +1,5 @@
 export const AIRTABLE_BASE = 'appp3Khl8aGLWR2xx';
-export const AIRTABLE_API_KEY = 'key7jIbxK4zBiOYDr';
+export const AIRTABLE_API_KEY = 'patgsze65pbQRLOlH.8db41a5d8e901744b15ab77be603e07439cceaaa5a099b0b56639e78542cdfb6';
 export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/embed/shrQcROm2BssWQbFv';
 
 export const MAPBOX_STYLE = 'mapbox://styles/atlasmedliq/ckiodibg82x1k17tcof8e9pmd';
